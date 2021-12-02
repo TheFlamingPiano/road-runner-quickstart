f8789131b570690f5dd4b7236d46f2769bb6528b
Cyrus Lowden <cylowden@outlook.com>
2021-12-01 19:23:40 -0600

    disable more unused opmodes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/BlueDuckAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/BlueDuckAuto.java
@@ -3,6 +3,7 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.snappy.SnappyHardware;
@@ -13,6 +14,7 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuild
 /*
  * This is an example of a more complex path to really test the tuning.
  */
+@Disabled
 @Autonomous(group = "drive")
 public class BlueDuckAuto extends LinearOpMode {
     final double EXTENSION_READY_DISTANCE = 0.0;
