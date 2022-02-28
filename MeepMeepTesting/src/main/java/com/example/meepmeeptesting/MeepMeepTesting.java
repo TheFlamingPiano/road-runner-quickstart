edc5fe70a49e0c32d5b38691715b5da9db3b0f4b
debadam <debadam@msn.com>
2022-02-27 23:41:50 -0600

    MeepMeep test with velocity contraint change

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,9 +1,18 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.profile.VelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.AngularVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.MinVelocityConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TrajectoryVelocityConstraint;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
@@ -14,15 +23,21 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 10)
                 .setDimensions(12.5, 16)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(10, 60, Math.toRadians(180)))
+                                .back(32)
+                                .setVelConstraint(getVelocityConstraint(5, Math.toRadians(180), 10))
+                                .back(10)
+                                .resetVelConstraint()
+                                .forward(42)
+                                .waitSeconds(1)
+//                                .forward(30)
+//                                .turn(Math.toRadians(90))
+//                                .forward(30)
+//                                .turn(Math.toRadians(90))
+//                                .forward(30)
+//                                .turn(Math.toRadians(90))
+//                                .forward(30)
+//                                .turn(Math.toRadians(90))
                                 .build()
                 );
 
@@ -31,5 +46,12 @@ public class MeepMeepTesting {
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
                 .start();
+    }
+
+    public static TrajectoryVelocityConstraint getVelocityConstraint(double maxVel, double maxAngularVel, double trackWidth) {
+        return new MinVelocityConstraint(Arrays.asList(
+                new AngularVelocityConstraint(maxAngularVel),
+                new MecanumVelocityConstraint(maxVel, trackWidth)
+        ));
     }
 }
