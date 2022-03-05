de6d88a88620eaf127b02a6065c6470eb0830d26
Cyrus Lowden <cylowden@outlook.com>
2022-03-04 20:03:42 -0600

    changed the position that we park to. WOOO READY FOR COMPETITION FOR REAL THIS TIME!!!! B) WERE COOL

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/snappy/BlueParkMultiBlock.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/snappy/BlueParkMultiBlock.java
@@ -31,7 +31,7 @@ import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
             Pose2d startPos = new Pose2d(10, 60, Math.toRadians(180));
             Pose2d startMod = new Pose2d(10,61.6,Math.toRadians(180));
-            Pose2d warehousePos = new Pose2d(41, 62.6, Math.toRadians(180));
+            Pose2d warehousePos = new Pose2d(41, 62.0, Math.toRadians(180));
             snappy.setPoseEstimate(startPos);
             telemetry.update();
             double angles[] = ik.getAngles(EXTENSION_READY_DISTANCE, EXTENSION_READY_HEIGHT);
@@ -93,7 +93,7 @@ cam.runOpMode(this,true, SnappyHardware.TeamColor.BLUE);
             for (int i = 0; i < 3; i++) {
                 snappy.deliverExtraBlock(SnappyHardware.TeamColor.BLUE, i, this, startPos, warehousePos, startMod);
             }
-                TrajectorySequence trajectory1 = snappy.trajectorySequenceBuilder(startPos)
+                TrajectorySequence trajectory1 = snappy.trajectorySequenceBuilder(snappy.getPoseEstimate())
                         .lineToLinearHeading(warehousePos)
 //                    .turn(Math.toRadians(90))
 //                    .strafeRight(5)
